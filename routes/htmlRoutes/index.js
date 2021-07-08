82d9667f14983990a06decfea47e6065d0809a26
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-07-07 17:15:25 -0700

    not working

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,17 +1,24 @@
-const path = require("path");
-const router = require("express").Router();
+const path = require('path');
+const router = require('express').Router();
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-  });
-  app.get("/animals", (req, res) => {
-      res.sendFile(path.join(__dirname, "./public/animals.html"));
-  });
-  app.get("/zookeepers", (req, res) => {
-      res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
-  });
-  app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "./public/index.html"));
-  });
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
 
-  module.exports = router;
+router.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/animals.html'));
+});
+
+router.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
+});
+
+router.get('/aquarium', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/aquarium.html'));
+});
+
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+module.exports = router;
