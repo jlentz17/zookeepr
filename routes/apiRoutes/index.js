5b38bcc8774b818b5e8b099c67fe216fedd81403
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-07-07 21:46:55 -0700

    edited text

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const animalRoutes = require('../apiRoutes/animalRoutes');
-const zookeeperRoutes = require('../apiRoutes/zookeeperRoutes');
+const animalRoutes = require('./animalRoutes');
+const zookeeperRoutes = require('./zookeeperRoutes');
 
 router.use(animalRoutes);
 router.use(zookeeperRoutes);
